cef5ac284a2b6a794159fba4635e29a56d452fcc
jumpcutfindo <danielhoe97@gmail.com>
2025-08-11 23:07:28 +0800

    feat: add some basic elements to app

--- apps/fanyi/src/renderer/App.tsx
+++ apps/fanyi/src/renderer/App.tsx
@@ -1,7 +1,30 @@
+import { Button } from '@renderer/components/ui/Button';
+import { Label } from '@renderer/components/ui/Label';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+} from '@renderer/components/ui/Select';
+
 function App() {
   return (
-    <div className="bg-red-500">
-      <h1>Hello world!</h1>
+    <div className="flex h-full flex-row">
+      <div className="flex w-72 flex-col gap-4 p-4">
+        <div className="flex flex-col gap-2">
+          <Label>Presets</Label>
+          <Select>
+            <SelectTrigger className="w-full">Trigger</SelectTrigger>
+            <SelectContent>
+              <SelectItem value="item-1">Item 1</SelectItem>
+              <SelectItem value="item-1">Item 2</SelectItem>
+              <SelectItem value="item-1">Item 3</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <Button variant="outline">Take Screenshot</Button>
+      </div>
+      <div className="h-full w-full"></div>
     </div>
   );
 }
