23bb35c40c0e792735652420ea3bddbd59abe1eb
jumpcutfindo <danielhoe97@gmail.com>
2025-08-12 22:14:59 +0800

    feat: add tanstack query to client

--- apps/fanyi/src/renderer/main.tsx
+++ apps/fanyi/src/renderer/main.tsx
@@ -1,17 +1,17 @@
 import './index.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from '@renderer/App';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
-
-// Use contextBridge
-window.ipcRenderer.on('main-process-message', (_event, message) => {
-  console.log(message);
-});
