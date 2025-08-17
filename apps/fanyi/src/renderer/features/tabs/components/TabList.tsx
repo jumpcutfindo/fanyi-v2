5b31cc60e9df0c670bde80c003eafefd8ca7b549
jumpcutfindo <danielhoe97@gmail.com>
2025-08-18 00:02:57 +0800

    feat: show indicator for active tab

--- apps/fanyi/src/renderer/features/tabs/components/TabList.tsx
+++ apps/fanyi/src/renderer/features/tabs/components/TabList.tsx
@@ -1,7 +1,10 @@
+import { cn } from '@renderer/lib/utils';
 import { Tab, useTabStore } from '@renderer/stores/useTabStore';
 
 export function TabList() {
   const tabs = useTabStore((state) => state.tabs);
+
+  const activeTab = useTabStore((state) => state.activeTab);
   const setActiveTab = useTabStore((state) => state.setActiveTab);
 
   if (tabs.length === 0) {
@@ -14,6 +17,7 @@ export function TabList() {
         <TabItem
           key={tab.id}
           tab={tab}
+          isActive={activeTab?.id === tab.id}
           handleSelect={() => setActiveTab(tab)}
         />
       ))}
@@ -23,14 +27,18 @@ export function TabList() {
 
 export interface TabItemProps {
   tab: Tab;
+  isActive: boolean;
   handleSelect: () => void;
 }
 
-export function TabItem({ tab, handleSelect }: TabItemProps) {
+export function TabItem({ tab, isActive, handleSelect }: TabItemProps) {
   return (
     <button
       type="button"
-      className="bg-card not-last:border-e-muted hover:bg-muted flex h-8 min-w-fit items-center truncate border-e border-e-transparent p-2 text-sm hover:cursor-pointer"
+      className={cn(
+        'bg-card not-last:border-e-muted hover:bg-muted flex h-8 min-w-fit items-center truncate border-e border-e-transparent p-2 text-sm hover:cursor-pointer',
+        isActive ? 'border-b-primary border-b' : ''
+      )}
       onClick={handleSelect}
     >
       {tab.title}
