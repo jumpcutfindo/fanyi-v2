4172048ebbaaf9259c6ba14138237835824bb520
jumpcutfindo <danielhoe97@gmail.com>
2025-08-21 22:57:55 +0800

    feat: disable tabs on editing

--- apps/fanyi/src/renderer/features/tabs/components/TabList.tsx
+++ apps/fanyi/src/renderer/features/tabs/components/TabList.tsx
@@ -1,9 +1,12 @@
 import { X } from 'lucide-react';
 
 import { cn } from '@renderer/lib/utils';
+import { useSidebarStore } from '@renderer/stores/useSidebarStore';
 import { Tab, useTabStore } from '@renderer/stores/useTabStore';
 
 export function TabList() {
+  const sidebarState = useSidebarStore((state) => state.sidebarState);
+
   const tabs = useTabStore((state) => state.tabs);
 
   const activeTab = useTabStore((state) => state.activeTab);
@@ -22,6 +25,7 @@ export function TabList() {
           isActive={activeTab?.id === tab.id}
           handleSelect={() => setActiveTab(tab)}
           handleClose={() => removeTab(tab.id)}
+          disabled={sidebarState.state === 'editor'}
         />
       ))}
       {previewTab ? (
@@ -31,6 +35,7 @@ export function TabList() {
           handleSelect={() => setActiveTab(previewTab)}
           handleClose={() => removeTab(previewTab.id)}
           isPreview
+          disabled={sidebarState.state === 'editor'}
         />
       ) : null}
     </div>
@@ -43,6 +48,7 @@ export interface TabItemProps {
   isPreview?: boolean;
   handleSelect: () => void;
   handleClose: () => void;
+  disabled?: boolean;
 }
 
 export function TabItem({
@@ -51,28 +57,34 @@ export function TabItem({
   isPreview,
   handleSelect,
   handleClose,
+  disabled,
 }: TabItemProps) {
   return (
     <div className="group relative w-full">
       <button
         type="button"
         className={cn(
-          'bg-card not-last:border-e-muted hover:bg-muted flex h-8 w-full items-center truncate border-e border-e-transparent p-2 text-sm hover:cursor-pointer',
-          isActive ? 'border-b-primary border-b' : ''
+          'bg-card not-last:border-e-muted hover:bg-muted flex h-8 w-full items-center truncate border-e border-e-transparent p-2 text-sm',
+          disabled ? 'cursor-not-allowed' : 'cursor-pointer',
+          disabled && !isPreview ? 'opacity-20' : '',
+          isActive ? 'border-b-primary border-b' : 'opacity-50'
         )}
         onClick={handleSelect}
+        disabled={disabled}
       >
         <span className={cn('max-w-[90%] truncate', isPreview ? 'italic' : '')}>
           {tab.title}
         </span>
       </button>
 
-      <button
-        className="hover:bg-muted absolute top-[50%] right-2 translate-y-[-50%] cursor-pointer rounded-full opacity-0 group-hover:opacity-100"
-        onClick={handleClose}
-      >
-        <X className="size-4" />
-      </button>
+      {!disabled ? (
+        <button
+          className="hover:bg-muted absolute top-[50%] right-2 translate-y-[-50%] cursor-pointer rounded-full opacity-0 group-hover:opacity-100"
+          onClick={handleClose}
+        >
+          <X className="size-4" />
+        </button>
+      ) : null}
     </div>
   );
 }
