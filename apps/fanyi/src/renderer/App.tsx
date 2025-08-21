4d07d1162c9ae36499b7eb5549c8df1fa4a24288
jumpcutfindo <danielhoe97@gmail.com>
2025-08-21 22:50:12 +0800

    chore: remove react transition group

--- apps/fanyi/src/renderer/App.tsx
+++ apps/fanyi/src/renderer/App.tsx
@@ -1,5 +1,4 @@
 import { useRef } from 'react';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import { PresetEditor } from '@renderer/features/screenshot/components/PresetEditor';
 import { PresetManager } from '@renderer/features/screenshot/components/PresetManager';
@@ -31,21 +30,9 @@ function App() {
   return (
     <div className="flex h-full flex-row">
       <div className="relative h-full min-w-70 gap-4">
-        <TransitionGroup>
-          <CSSTransition
-            key={sidebarState.state}
-            nodeRef={sidebarRef}
-            timeout={200}
-            classNames="sidebar"
-          >
-            <div
-              ref={sidebarRef}
-              className="absolute h-full w-full transition-all"
-            >
-              {renderSidebar()}
-            </div>
-          </CSSTransition>
-        </TransitionGroup>
+        <div ref={sidebarRef} className="absolute h-full w-full">
+          {renderSidebar()}
+        </div>
       </div>
       <div className="bg-muted flex h-full w-0 grow flex-col">
         <TabList />
