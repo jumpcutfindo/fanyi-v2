4525359d18a2e3ab6706cd60e4e0f39254cb3a91
jumpcutfindo <danielhoe97@gmail.com>
2025-08-24 01:43:15 +0800

    chore: split display tabs into separate files

--- apps/fanyi/src/renderer/features/tabs/components/TabDisplay.tsx
+++ apps/fanyi/src/renderer/features/tabs/components/TabDisplay.tsx
@@ -1,12 +1,6 @@
-import { ScreenshotPreset } from '@shared/types/screenshot';
-import { Loader2Icon, Pencil, Play } from 'lucide-react';
-
-import { useGetOcrWithPresetQuery } from '@renderer/features/screenshot/queries/getOcrWithPreset.query';
-import { useGetScreenshotWithPreset } from '@renderer/features/screenshot/queries/getScreenshotWithPreset.query';
-import { TranslationList } from '@renderer/features/translation/components/TranslationList';
-import { cn } from '@renderer/lib/utils';
-import { useSidebarStore } from '@renderer/stores/useSidebarStore';
-import { TranslationTab, useTabStore } from '@renderer/stores/useTabStore';
+import { PreviewTabDisplay } from '@renderer/features/tabs/components/PreviewTabDisplay';
+import { TranslationTabDisplay } from '@renderer/features/tabs/components/TranslationTabDisplay';
+import { useTabStore } from '@renderer/stores/useTabStore';
 
 const containerStyle = 'flex grow items-center justify-center';
 
@@ -35,107 +29,6 @@ export function TabDisplay() {
   return (
     <div className={containerStyle}>
       {activeTab.title} ({activeTab.id}) selected
-    </div>
-  );
-}
-
-interface PreviewTabDisplayProps {
-  preset: ScreenshotPreset;
-}
-
-function PreviewTabDisplay({ preset }: PreviewTabDisplayProps) {
-  const { data: screenshot, isPending: isScreenshotPending } =
-    useGetScreenshotWithPreset(preset);
-
-  const sidebarState = useSidebarStore((state) => state.sidebarState);
-  const setSidebarState = useSidebarStore((state) => state.setSidebarState);
-
-  const addTab = useTabStore((state) => state.addTab);
-
-  const showMetadataAndButtons = sidebarState.state !== 'editor';
-
-  return (
-    <div className="flex h-full w-full flex-col items-center justify-center space-y-4">
-      {!isScreenshotPending ? (
-        <img
-          src={screenshot}
-          className={cn('h-120 w-full bg-black/20 object-scale-down')}
-        />
-      ) : (
-        <Loader2Icon className="animate-spin" />
-      )}
-      {showMetadataAndButtons ? (
-        <>
-          <div className="text-center">
-            <h1 className="text-lg font-semibold">{preset.name}</h1>
-            <span>
-              {preset.options.crop?.width}x{preset.options.crop?.height}
-            </span>
-          </div>
-          <div className="flex grow flex-row items-center justify-center gap-8 px-32">
-            <button
-              className="group flex size-12 cursor-pointer items-center justify-center rounded-full bg-green-200 hover:bg-green-300"
-              onClick={() => {
-                if (!screenshot) {
-                  return;
-                }
-
-                addTab(
-                  {
-                    id: '',
-                    type: 'translation',
-                    title: preset.name,
-                    preset: preset,
-                    screenshot,
-                  },
-                  { setActive: true }
-                );
-              }}
-              disabled={!screenshot}
-            >
-              <Play className="text-green-600 group-hover:fill-green-600" />
-            </button>
-            <button
-              className="group flex size-12 cursor-pointer items-center justify-center rounded-full bg-amber-200 hover:bg-amber-300"
-              onClick={() =>
-                setSidebarState({
-                  state: 'editor',
-                  options: { mode: 'edit', initialPreset: preset },
-                })
-              }
-            >
-              <Pencil className="text-amber-600 group-hover:fill-amber-600" />
-            </button>
-          </div>
-        </>
-      ) : null}
-    </div>
-  );
-}
-
-interface TranslationTabDisplayProps {
-  tab: TranslationTab;
-}
-
-function TranslationTabDisplay({ tab }: TranslationTabDisplayProps) {
-  const { id, preset, screenshot } = tab;
-
-  const { data: ocrText, isPending: isOcrTextPending } =
-    useGetOcrWithPresetQuery(id, preset);
-
-  return (
-    <div className="flex h-full w-full flex-col items-center justify-center space-y-4">
-      {isOcrTextPending ? (
-        <Loader2Icon className="animate-spin" />
-      ) : (
-        <>
-          <img
-            src={screenshot}
-            className={cn('h-120 w-full bg-black/20 object-scale-down')}
-          />
-          <TranslationList translations={ocrText} />
-        </>
-      )}
     </div>
   );
 }
---
+++ apps/fanyi/src/renderer/features/tabs/components/TranslationTabDisplay.tsx
@@ -0,0 +1,33 @@
+import { Loader2Icon } from 'lucide-react';
+
+import { useGetOcrWithPresetQuery } from '@renderer/features/screenshot/queries/getOcrWithPreset.query';
+import { TranslationList } from '@renderer/features/translation/components/TranslationList';
+import { cn } from '@renderer/lib/utils';
+import { TranslationTab } from '@renderer/stores/useTabStore';
+
+interface TranslationTabDisplayProps {
+  tab: TranslationTab;
+}
+
+export function TranslationTabDisplay({ tab }: TranslationTabDisplayProps) {
+  const { id, preset, screenshot } = tab;
+
+  const { data: ocrText, isPending: isOcrTextPending } =
+    useGetOcrWithPresetQuery(id, preset);
+
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center space-y-4">
+      {isOcrTextPending ? (
+        <Loader2Icon className="animate-spin" />
+      ) : (
+        <>
+          <img
+            src={screenshot}
+            className={cn('h-120 w-full bg-black/20 object-scale-down')}
+          />
+          <TranslationList translations={ocrText} />
+        </>
+      )}
+    </div>
+  );
+}
