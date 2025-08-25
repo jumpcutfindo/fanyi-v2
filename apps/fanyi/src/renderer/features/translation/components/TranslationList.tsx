7d27415dbce6d45e25865b94ec1ebf825f584e4a
jumpcutfindo <danielhoe97@gmail.com>
2025-08-25 23:26:18 +0800

    feat: filter results to unique values

--- apps/fanyi/src/renderer/features/translation/components/TranslationList.tsx
+++ apps/fanyi/src/renderer/features/translation/components/TranslationList.tsx
@@ -1,6 +1,8 @@
 import { DictionaryEntry } from '@shared/types/dictionary';
 import { OcrResult } from '@shared/types/ocr';
 
+import { Button } from '@renderer/components/ui/Button';
+
 interface TranslationListProps {
   ocrResult: OcrResult;
   translations: DictionaryEntry[];
@@ -10,11 +12,26 @@ export function TranslationList({
   ocrResult,
   translations,
 }: TranslationListProps) {
+  const entrySet = new Set(translations);
+  const uniqueEntries = [...entrySet];
+
   return (
-    <div className="flex h-0 w-full grow flex-col items-center gap-2 overflow-auto py-4">
-      {translations.map((t, index) => (
-        <TranslationItem key={`${t.simplified}+${index}`} entry={t} />
-      ))}
+    <div className="flex h-0 w-full grow flex-row">
+      <div className="flex h-full w-60 flex-row flex-wrap gap-1 overflow-auto p-2">
+        {uniqueEntries.map((t) => (
+          <Button
+            variant="outline"
+            className="flex-1 px-2 py-1 text-lg font-normal"
+          >
+            {t.simplified}
+          </Button>
+        ))}
+      </div>
+      <div className="flex grow flex-col items-center gap-2 overflow-auto p-2">
+        {uniqueEntries.map((t, index) => (
+          <TranslationItem key={`${t.simplified}+${index}`} entry={t} />
+        ))}
+      </div>
     </div>
   );
 }
@@ -25,7 +42,7 @@ interface TranslationItemProps {
 
 function TranslationItem({ entry }: TranslationItemProps) {
   return (
-    <div className="bg-card flex w-[70%] flex-col rounded-md border p-4">
+    <div className="bg-card flex w-full flex-col rounded-md border p-4">
       <span className="text-2xl">{entry.simplified}</span>
       <span className="text-sm">{entry.pinyin}</span>
       <div>{entry.definition}</div>
