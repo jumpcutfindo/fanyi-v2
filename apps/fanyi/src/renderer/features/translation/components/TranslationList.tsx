279c618f87171f2872636dc9b6ec9ebd7210b626
jumpcutfindo <danielhoe97@gmail.com>
2025-08-26 23:23:33 +0800

    feat: add scroll to translation on button click

--- apps/fanyi/src/renderer/features/translation/components/TranslationList.tsx
+++ apps/fanyi/src/renderer/features/translation/components/TranslationList.tsx
@@ -1,7 +1,10 @@
 import { DictionaryEntry } from '@shared/types/dictionary';
 import { OcrResult } from '@shared/types/ocr';
+import { useRef } from 'react';
 
 import { Button } from '@renderer/components/ui/Button';
+
+const highlightClass = ['border-primary', 'bg-primary/10'];
 
 interface TranslationListProps {
   ocrResult: OcrResult;
@@ -12,7 +15,27 @@ export function TranslationList({
   ocrResult,
   translations,
 }: TranslationListProps) {
+  const wordToTranslationRef = useRef<Record<string, HTMLDivElement>>({});
+
   const uniqueEntries = [...new Set(translations)];
+
+  const scrollToEntry = (word: string) => {
+    const element = wordToTranslationRef.current[word];
+
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+
+      requestAnimationFrame(() => {
+        // Add highlight to element
+        element.classList.add(...highlightClass);
+
+        // Remove after 1 second
+        setTimeout(() => {
+          element.classList.remove(...highlightClass);
+        }, 1500);
+      });
+    }
+  };
 
   return (
     <div className="flex h-0 w-full grow flex-row">
@@ -20,8 +43,10 @@ export function TranslationList({
         <div className="flex h-fit flex-row flex-wrap">
           {uniqueEntries.map((t) => (
             <Button
+              key={t.simplified}
               variant="outline"
               className="flex-1 px-2 py-1 text-lg font-normal"
+              onClick={() => scrollToEntry(t.simplified)}
             >
               {t.simplified}
             </Button>
@@ -30,7 +55,15 @@ export function TranslationList({
       </div>
       <div className="flex grow flex-col items-center gap-2 overflow-auto p-2">
         {uniqueEntries.map((t, index) => (
-          <TranslationItem key={`${t.simplified}+${index}`} entry={t} />
+          <TranslationItem
+            ref={(ref) => {
+              if (ref) {
+                wordToTranslationRef.current[t.simplified] = ref;
+              }
+            }}
+            key={`${t.simplified}+${index}`}
+            entry={t}
+          />
         ))}
       </div>
     </div>
@@ -38,12 +71,16 @@ export function TranslationList({
 }
 
 interface TranslationItemProps {
+  ref?: React.Ref<HTMLDivElement>;
   entry: DictionaryEntry;
 }
 
-function TranslationItem({ entry }: TranslationItemProps) {
+function TranslationItem({ ref, entry }: TranslationItemProps) {
   return (
-    <div className="bg-card flex w-full flex-col rounded-md border p-4">
+    <div
+      ref={ref}
+      className="bg-card flex w-full flex-col rounded-md border p-4 transition-all"
+    >
       <span className="text-2xl">{entry.simplified}</span>
       <span className="text-sm">{entry.pinyin}</span>
       <div>{entry.definition}</div>
