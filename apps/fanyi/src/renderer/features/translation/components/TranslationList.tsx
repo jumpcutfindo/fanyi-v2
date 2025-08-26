9ea57c96ac63ca5da8ca38369e5e89f6e6d2c33c
jumpcutfindo <danielhoe97@gmail.com>
2025-08-26 23:17:14 +0800

    style: improve button display

--- apps/fanyi/src/renderer/features/translation/components/TranslationList.tsx
+++ apps/fanyi/src/renderer/features/translation/components/TranslationList.tsx
@@ -12,20 +12,21 @@ export function TranslationList({
   ocrResult,
   translations,
 }: TranslationListProps) {
-  const entrySet = new Set(translations);
-  const uniqueEntries = [...entrySet];
+  const uniqueEntries = [...new Set(translations)];
 
   return (
     <div className="flex h-0 w-full grow flex-row">
-      <div className="flex h-full w-60 flex-row flex-wrap gap-1 overflow-auto p-2">
-        {uniqueEntries.map((t) => (
-          <Button
-            variant="outline"
-            className="flex-1 px-2 py-1 text-lg font-normal"
-          >
-            {t.simplified}
-          </Button>
-        ))}
+      <div className="flex h-full w-60 overflow-auto p-2">
+        <div className="flex h-fit flex-row flex-wrap">
+          {uniqueEntries.map((t) => (
+            <Button
+              variant="outline"
+              className="flex-1 px-2 py-1 text-lg font-normal"
+            >
+              {t.simplified}
+            </Button>
+          ))}
+        </div>
       </div>
       <div className="flex grow flex-col items-center gap-2 overflow-auto p-2">
         {uniqueEntries.map((t, index) => (
