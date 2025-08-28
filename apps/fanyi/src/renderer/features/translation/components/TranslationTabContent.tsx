a086bf48c04561d8c45a94f2d4385abd1ec34e44
jumpcutfindo <danielhoe97@gmail.com>
2025-08-29 00:12:45 +0800

    feat: add image resizing in translation preview

--- apps/fanyi/src/renderer/features/translation/components/TranslationTabContent.tsx
+++ apps/fanyi/src/renderer/features/translation/components/TranslationTabContent.tsx
@@ -1,5 +1,12 @@
-import { Loader2Icon } from 'lucide-react';
+import { Ban, Image, ImageUpscale, Loader2Icon } from 'lucide-react';
+import { useState } from 'react';
 
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '@renderer/components/ui/Tabs';
 import { useGetOcrWithPresetQuery } from '@renderer/features/screenshot/queries/getOcrWithPreset.query';
 import { TranslationList } from '@renderer/features/translation/components/TranslationList';
 import { cn } from '@renderer/lib/utils';
@@ -23,6 +30,8 @@ export function TranslationTabContent({ tab }: TranslationTabContentProps) {
   const { data: ocrResponse, isPending: isOcrTextPending } =
     useGetOcrWithPresetQuery(id, preset);
 
+  const [isTranslationsHidden, setIsTranslationsHidden] = useState(false);
+
   return (
     <div className="flex h-full w-full flex-col items-center justify-center">
       {isOcrTextPending ? (
@@ -38,18 +47,89 @@ export function TranslationTabContent({ tab }: TranslationTabContentProps) {
         </div>
       ) : (
         <div className="flex h-full w-full flex-col">
-          <img
-            src={screenshot}
-            className={cn('h-40 w-full bg-black/20 object-scale-down')}
+          <PreviewImage
+            screenshot={screenshot}
+            setTranslationsHidden={setIsTranslationsHidden}
           />
-          <TranslationList
-            ocrResult={ocrResponse.ocrResult}
-            translations={ocrResponse.translations}
-            activeWord={activeWord}
-            setActiveWord={setActiveWord}
-          />
+          {!isTranslationsHidden ? (
+            <TranslationList
+              ocrResult={ocrResponse.ocrResult}
+              translations={ocrResponse.translations}
+              activeWord={activeWord}
+              setActiveWord={setActiveWord}
+            />
+          ) : null}
         </div>
       )}
+    </div>
+  );
+}
+
+type ImageSize = 'none' | 'small' | 'medium' | 'large' | 'full';
+
+interface PreviewImageProps {
+  screenshot: string;
+
+  setTranslationsHidden: (hidden: boolean) => void;
+}
+
+function PreviewImage({
+  screenshot,
+  setTranslationsHidden,
+}: PreviewImageProps) {
+  const [imageSize, setImageSize] = useState<ImageSize>('small');
+
+  const getImageSizeClass = () => {
+    setTranslationsHidden(false);
+    switch (imageSize) {
+      case 'none':
+        return 'hidden';
+      case 'small':
+        return 'h-40 lg:h-60';
+      case 'medium':
+        return 'h-80 lg:h-120';
+      case 'large':
+        return 'h-120 lg:h-160';
+      case 'full':
+        setTranslationsHidden(true);
+        return 'h-[90vh]';
+    }
+  };
+
+  return (
+    <div
+      className={cn(
+        'flex w-full flex-col items-center justify-center gap-2 bg-black/20 p-2',
+        imageSize === 'full' ? 'h-full' : ''
+      )}
+    >
+      <img
+        src={screenshot}
+        className={cn('object-scale-down', getImageSizeClass())}
+      />
+      <Tabs
+        className="w-full items-center transition-all"
+        value={imageSize}
+        onValueChange={(value) => setImageSize(value as ImageSize)}
+      >
+        <TabsList>
+          <TabsTrigger value="none">
+            <Ban />
+          </TabsTrigger>
+          <TabsTrigger value="small">
+            <Image className="size-3" />
+          </TabsTrigger>
+          <TabsTrigger value="medium">
+            <Image className="size-4" />
+          </TabsTrigger>
+          <TabsTrigger value="large">
+            <Image className="size-5" />
+          </TabsTrigger>
+          <TabsTrigger value="full">
+            <ImageUpscale className="size-5" />
+          </TabsTrigger>
+        </TabsList>
+      </Tabs>
     </div>
   );
 }
