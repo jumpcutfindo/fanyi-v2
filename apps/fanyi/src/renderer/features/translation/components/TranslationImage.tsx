365b0f4551b59e42167cee40ff29cd077449234e
jumpcutfindo <danielhoe97@gmail.com>
2025-08-29 00:42:02 +0800

    chore: extract image to own component

---
+++ apps/fanyi/src/renderer/features/translation/components/TranslationImage.tsx
@@ -0,0 +1,94 @@
+import { Ban, Copy, Image, ImageUpscale } from 'lucide-react';
+import { toast } from 'sonner';
+
+import { Button } from '@renderer/components/ui/Button';
+import { Tabs, TabsList, TabsTrigger } from '@renderer/components/ui/Tabs';
+import { cn } from '@renderer/lib/utils';
+import { TranslationTab } from '@renderer/stores/useTabStore';
+import { imageBase64ToBlob } from '@renderer/utils/image.util';
+
+type ImageSize = TranslationTab['imageSize'];
+
+interface TranslationImageProps {
+  screenshot: string;
+  setTranslationsHidden: (hidden: boolean) => void;
+
+  imageSize: ImageSize;
+  setImageSize: (size: ImageSize) => void;
+}
+
+export function TranslationImage({
+  screenshot,
+  setTranslationsHidden,
+  imageSize,
+  setImageSize,
+}: TranslationImageProps) {
+  const getImageSizeClass = () => {
+    setTranslationsHidden(false);
+    switch (imageSize) {
+      case 'none':
+        return 'hidden';
+      case 'small':
+        return 'h-40 lg:h-60';
+      case 'medium':
+        return 'h-80 lg:h-120';
+      case 'large':
+        return 'h-120 lg:h-160';
+      case 'full':
+        setTranslationsHidden(true);
+        return 'h-[90vh]';
+    }
+  };
+
+  const handleCopyImage = () => {
+    const clipboardItem = new ClipboardItem({
+      'image/png': imageBase64ToBlob(screenshot),
+    });
+
+    navigator.clipboard.write([clipboardItem]).then(() => {
+      toast.success('Image copied to clipboard');
+    });
+  };
+
+  return (
+    <div
+      className={cn(
+        'flex w-full flex-col items-center justify-center gap-2 bg-black/20 p-2',
+        imageSize === 'full' ? 'h-full' : ''
+      )}
+    >
+      <img
+        src={screenshot}
+        className={cn('object-scale-down', getImageSizeClass())}
+      />
+      <div className="flex flex-row gap-2">
+        <Tabs
+          className="w-full items-center transition-all"
+          value={imageSize}
+          onValueChange={(value) => setImageSize(value as ImageSize)}
+        >
+          <TabsList>
+            <TabsTrigger value="none">
+              <Ban />
+            </TabsTrigger>
+            <TabsTrigger value="small">
+              <Image className="size-3" />
+            </TabsTrigger>
+            <TabsTrigger value="medium">
+              <Image className="size-4" />
+            </TabsTrigger>
+            <TabsTrigger value="large">
+              <Image className="size-5" />
+            </TabsTrigger>
+            <TabsTrigger value="full">
+              <ImageUpscale className="size-5" />
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
+        <Button variant="secondary" onClick={handleCopyImage} type="button">
+          <Copy />
+        </Button>
+      </div>
+    </div>
+  );
+}
--- apps/fanyi/src/renderer/features/translation/components/TranslationTabContent.tsx
+++ apps/fanyi/src/renderer/features/translation/components/TranslationTabContent.tsx
@@ -1,16 +1,11 @@
-import { Ban, Copy, Image, ImageUpscale, Loader2Icon } from 'lucide-react';
+import { Loader2Icon } from 'lucide-react';
 import { useState } from 'react';
-import { toast } from 'sonner';
 
-import { Button } from '@renderer/components/ui/Button';
-import { Tabs, TabsList, TabsTrigger } from '@renderer/components/ui/Tabs';
 import { useGetOcrWithPresetQuery } from '@renderer/features/screenshot/queries/getOcrWithPreset.query';
+import { TranslationImage } from '@renderer/features/translation/components/TranslationImage';
 import { TranslationList } from '@renderer/features/translation/components/TranslationList';
 import { cn } from '@renderer/lib/utils';
 import { TranslationTab, useTabStore } from '@renderer/stores/useTabStore';
-import { imageBase64ToBlob } from '@renderer/utils/image.util';
-
-type ImageSize = 'none' | 'small' | 'medium' | 'large' | 'full';
 
 interface TranslationTabContentProps {
   tab: TranslationTab;
@@ -27,7 +22,7 @@ export function TranslationTabContent({ tab }: TranslationTabContentProps) {
     });
   };
 
-  const setImageSize = (size: ImageSize) => {
+  const setImageSize = (size: typeof imageSize) => {
     updateTab({
       ...tab,
       preset: {
@@ -57,7 +52,7 @@ export function TranslationTabContent({ tab }: TranslationTabContentProps) {
         </div>
       ) : (
         <div className="flex h-full w-full flex-col">
-          <PreviewImage
+          <TranslationImage
             screenshot={screenshot}
             setTranslationsHidden={setIsTranslationsHidden}
             imageSize={imageSize}
@@ -73,90 +68,6 @@ export function TranslationTabContent({ tab }: TranslationTabContentProps) {
           ) : null}
         </div>
       )}
-    </div>
-  );
-}
-
-interface PreviewImageProps {
-  screenshot: string;
-  setTranslationsHidden: (hidden: boolean) => void;
-
-  imageSize: ImageSize;
-  setImageSize: (size: ImageSize) => void;
-}
-
-function PreviewImage({
-  screenshot,
-  setTranslationsHidden,
-  imageSize,
-  setImageSize,
-}: PreviewImageProps) {
-  const getImageSizeClass = () => {
-    setTranslationsHidden(false);
-    switch (imageSize) {
-      case 'none':
-        return 'hidden';
-      case 'small':
-        return 'h-40 lg:h-60';
-      case 'medium':
-        return 'h-80 lg:h-120';
-      case 'large':
-        return 'h-120 lg:h-160';
-      case 'full':
-        setTranslationsHidden(true);
-        return 'h-[90vh]';
-    }
-  };
-
-  const handleCopyImage = () => {
-    const clipboardItem = new ClipboardItem({
-      'image/png': imageBase64ToBlob(screenshot),
-    });
-
-    navigator.clipboard.write([clipboardItem]).then(() => {
-      toast.success('Image copied to clipboard');
-    });
-  };
-
-  return (
-    <div
-      className={cn(
-        'flex w-full flex-col items-center justify-center gap-2 bg-black/20 p-2',
-        imageSize === 'full' ? 'h-full' : ''
-      )}
-    >
-      <img
-        src={screenshot}
-        className={cn('object-scale-down', getImageSizeClass())}
-      />
-      <div className="flex flex-row gap-2">
-        <Tabs
-          className="w-full items-center transition-all"
-          value={imageSize}
-          onValueChange={(value) => setImageSize(value as ImageSize)}
-        >
-          <TabsList>
-            <TabsTrigger value="none">
-              <Ban />
-            </TabsTrigger>
-            <TabsTrigger value="small">
-              <Image className="size-3" />
-            </TabsTrigger>
-            <TabsTrigger value="medium">
-              <Image className="size-4" />
-            </TabsTrigger>
-            <TabsTrigger value="large">
-              <Image className="size-5" />
-            </TabsTrigger>
-            <TabsTrigger value="full">
-              <ImageUpscale className="size-5" />
-            </TabsTrigger>
-          </TabsList>
-        </Tabs>
-        <Button variant="secondary" onClick={handleCopyImage} type="button">
-          <Copy />
-        </Button>
-      </div>
     </div>
   );
 }
