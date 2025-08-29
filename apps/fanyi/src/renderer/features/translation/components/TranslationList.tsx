7bb9686e46ab880c4527b2bd076a7ccf7b06153b
jumpcutfindo <danielhoe97@gmail.com>
2025-08-29 23:33:53 +0800

    feat: add virtual window to improve rendering

--- apps/fanyi/src/renderer/features/translation/components/TranslationList.tsx
+++ apps/fanyi/src/renderer/features/translation/components/TranslationList.tsx
@@ -1,13 +1,9 @@
 import { DictionaryEntry } from '@shared/types/dictionary';
 import { OcrResult } from '@shared/types/ocr';
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useMemo, useRef, useState } from 'react';
+import { List, RowComponentProps, useListRef } from 'react-window';
 
 import { Button } from '@renderer/components/ui/Button';
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from '@renderer/components/ui/Tooltip';
 import { cn } from '@renderer/lib/utils';
 
 const highlightClass = ['border-primary', 'bg-primary/10'];
@@ -18,11 +14,8 @@ interface TranslationListProps {
 }
 
 export function TranslationList({ translations }: TranslationListProps) {
-  const translationItemsContainerRef = useRef<HTMLDivElement>(null);
-
-  const wordToButtonRef = useRef<Record<string, HTMLButtonElement>>({});
-  const wordToTranslationRef = useRef<Record<string, HTMLDivElement>>({});
-  const isObserverDisabled = useRef<boolean>(false);
+  const virtualListRef = useListRef(null);
+  const itemsRef = useRef<Record<string, HTMLDivElement>>({});
 
   const uniqueEntries = useMemo(
     () => [...new Set(translations)],
@@ -30,81 +23,67 @@ export function TranslationList({ translations }: TranslationListProps) {
   );
 
   const [activeWord, setActiveWord] = useState<string | null>(null);
-
   const [hoveredEntry, setHoveredEntry] = useState<DictionaryEntry | null>(
     null
   );
+  const visibleIndices = useRef<{
+    start: number;
+    end: number;
+  }>({
+    start: 0,
+    end: 0,
+  });
 
-  const scrollToEntry = (
-    word: string,
-    opts?: { behavior?: ScrollBehavior }
-  ) => {
-    const element = wordToTranslationRef.current[word];
-
-    if (element) {
-      // Set active word and prevent observer from interfering
-      setActiveWord(word);
-      isObserverDisabled.current = true;
-
-      element.scrollIntoView({
-        behavior: opts?.behavior || 'smooth',
-        block: 'center',
-      });
-
-      requestAnimationFrame(() => {
-        // Add highlight to element
-        element.classList.add(...highlightClass);
-
-        // Remove after some delay
-        setTimeout(() => {
-          element.classList.remove(...highlightClass);
-          isObserverDisabled.current = false;
-        }, 1500);
+  const scrollToEntry = (index: number) => {
+    if (virtualListRef.current) {
+      virtualListRef.current.scrollToRow({
+        index,
+        align: 'center',
+        behavior: 'smooth',
       });
     }
-  };
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        if (isObserverDisabled.current) return;
+    // Handle highlighting
+    // We need some delay before adding the highlight due to the virtual list
+    //
+    // The delay is calculated based on the distance from current position to
+    // the new position
+    // - If the index lies within the visible range, the delay is 0
+    // - If the index lies outside the visible range, the delay is calculated
 
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            // Find the corresponding word from our uniqueEntries
-            const intersectingWord = uniqueEntries.find(
-              (t) => wordToTranslationRef.current[t.simplified] === entry.target
-            );
-            if (intersectingWord) {
-              setActiveWord(intersectingWord.simplified);
-            }
-          }
+    const hasHighlightDelay =
+      index <= visibleIndices.current.start ||
+      index > visibleIndices.current.end;
+
+    const highlightDelay = hasHighlightDelay
+      ? Math.min(
+          Math.max(
+            Math.abs(
+              (visibleIndices.current.end + visibleIndices.current.start) / 2 -
+                index
+            ) * 20,
+            500
+          ),
+          900
+        )
+      : 0;
+
+    setTimeout(() => {
+      if (itemsRef.current[uniqueEntries[index].simplified]) {
+        const element = itemsRef.current[uniqueEntries[index].simplified];
+
+        requestAnimationFrame(() => {
+          // Add highlight to element
+          element.classList.add(...highlightClass);
+
+          // Remove after some delay
+          setTimeout(() => {
+            element.classList.remove(...highlightClass);
+          }, 1500);
         });
-      },
-      {
-        root: translationItemsContainerRef.current,
-        rootMargin: '-45% 0px -45% 0px', // Margin in the centre
-        threshold: 0,
       }
-    );
-
-    // Observe each translation item
-    Object.values(wordToTranslationRef.current).forEach((el) => {
-      observer.observe(el);
-    });
-
-    // Cleanup function
-    return () => {
-      observer.disconnect();
-    };
-  }, [uniqueEntries]);
-
-  useEffect(() => {
-    if (activeWord && wordToButtonRef.current[activeWord]) {
-      const el = wordToButtonRef.current[activeWord];
-      el.scrollIntoView({ behavior: 'smooth', block: 'center' });
-    }
-  }, [activeWord]);
+    }, highlightDelay);
+  };
 
   return (
     <div className="flex h-0 w-full grow flex-row">
@@ -119,19 +98,17 @@ export function TranslationList({ translations }: TranslationListProps) {
         </div>
         <div className="grow overflow-auto">
           <div className="flex h-fit flex-row flex-wrap">
-            {uniqueEntries.map((t) => (
+            {uniqueEntries.map((t, index) => (
               <Button
-                ref={(ref) => {
-                  if (ref) {
-                    wordToButtonRef.current[t.simplified] = ref;
-                  }
-                }}
                 variant="outline"
                 className={cn(
                   'flex-1 px-2 py-1 text-lg font-normal',
                   activeWord === t.simplified ? 'border-primary' : ''
                 )}
-                onClick={() => scrollToEntry(t.simplified)}
+                onClick={() => {
+                  setActiveWord(t.simplified);
+                  scrollToEntry(index);
+                }}
                 onMouseOver={() => setHoveredEntry(t)}
                 onMouseOut={() => setHoveredEntry(null)}
               >
@@ -141,40 +118,58 @@ export function TranslationList({ translations }: TranslationListProps) {
           </div>
         </div>
       </div>
-      <div
-        ref={translationItemsContainerRef}
-        className="flex grow flex-col items-center gap-2 overflow-auto p-2"
-      >
-        {uniqueEntries.map((t, index) => (
-          <TranslationItem
-            ref={(ref) => {
-              if (ref) {
-                wordToTranslationRef.current[t.simplified] = ref;
-              }
-            }}
-            key={`${t.simplified}+${index}`}
-            entry={t}
-          />
-        ))}
+      <div className="flex grow flex-col items-center gap-2 overflow-auto p-2">
+        <List
+          listRef={virtualListRef}
+          className="w-full"
+          rowComponent={TranslationItem}
+          rowCount={uniqueEntries.length}
+          rowHeight={112}
+          rowProps={{
+            setRef: (key: string, ref: HTMLDivElement) => {
+              if (!ref) return;
+              itemsRef.current[key] = ref;
+            },
+            entries: uniqueEntries,
+          }}
+          onRowsRendered={({ startIndex, stopIndex }) => {
+            visibleIndices.current = {
+              start: startIndex,
+              end: stopIndex,
+            };
+          }}
+        />
       </div>
     </div>
   );
 }
 
-interface TranslationItemProps {
-  ref?: React.Ref<HTMLDivElement>;
-  entry: DictionaryEntry;
-}
+type TranslationItemProps = RowComponentProps<{
+  setRef: (key: string, ref: HTMLDivElement) => void;
+  entries: DictionaryEntry[];
+}>;
 
-function TranslationItem({ ref, entry }: TranslationItemProps) {
+function TranslationItem({
+  setRef,
+  entries,
+  index,
+  style,
+}: TranslationItemProps) {
+  const entry = entries[index];
+
   return (
-    <div
-      ref={ref}
-      className="bg-card flex w-full flex-col rounded-md border p-4 transition-all"
-    >
-      <span className="text-2xl">{entry.simplified}</span>
-      <span className="text-sm">{entry.pinyin}</span>
-      <div>{entry.definition}</div>
+    <div className="mb-1" style={style}>
+      <div
+        ref={(ref) => {
+          if (!ref) return;
+          setRef(entry.simplified, ref);
+        }}
+        className="bg-card flex h-fit w-full flex-col rounded-md border p-4 transition-all"
+      >
+        <span className="text-2xl">{entry.simplified}</span>
+        <span className="text-sm">{entry.pinyin}</span>
+        <div>{entry.definition}</div>
+      </div>
     </div>
   );
 }
