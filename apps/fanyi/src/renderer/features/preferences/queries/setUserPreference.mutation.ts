daa40a0ccfcf9a56dd86062463f4e52be4ef48b9
jumpcutfindo <danielhoe97@gmail.com>
2025-09-12 23:58:53 +0800

    chore: invalidate preferences when updated

--- apps/fanyi/src/renderer/features/preferences/queries/setUserPreference.mutation.ts
+++ apps/fanyi/src/renderer/features/preferences/queries/setUserPreference.mutation.ts
@@ -1,13 +1,18 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { UserPreferences } from '@shared/types/preferences';
 
 export function useSetUserPreferenceMutation() {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationKey: ['set-user-preference'],
     mutationFn: (payload: {
       key: keyof UserPreferences;
       value: UserPreferences[keyof UserPreferences];
-    }) => window.api.setPreference(payload.key, payload.value),
+    }) => {
+      queryClient.invalidateQueries({ queryKey: ['user-preferences'] });
+      return window.api.setPreference(payload.key, payload.value);
+    },
   });
 }
