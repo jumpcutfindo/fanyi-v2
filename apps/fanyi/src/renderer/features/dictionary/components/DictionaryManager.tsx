474148b2b98f021e6298c5b9333764575af50d64
jumpcutfindo <danielhoe97@gmail.com>
2026-01-21 16:38:50 +0800

    feat: add dictionary page store

--- apps/fanyi/src/renderer/features/dictionary/components/DictionaryManager.tsx
+++ apps/fanyi/src/renderer/features/dictionary/components/DictionaryManager.tsx
@@ -1,10 +1,12 @@
 import { DictionaryMinimal } from '@shared/types/dictionary';
 import { SidebarContent, SidebarHeader } from '@renderer/components/Sidebar';
-import { Button } from '@renderer/components/ui/Button';
 import { useGetDictionaries } from '@renderer/features/dictionary/queries/getDictionaries.query';
 import { cn } from '@renderer/lib/utils';
+import { useDictionariesSidebarStore } from '@renderer/stores/useDictionariesSidebarStore';
 
 export function DictionaryManager() {
+  const { setSelectedDictionary } = useDictionariesSidebarStore();
+
   const { data: dictionaries } = useGetDictionaries();
 
   return (
@@ -12,7 +14,11 @@ export function DictionaryManager() {
       <SidebarHeader title="Dictionaries" />
       <SidebarContent>
         {dictionaries?.map((dictionary) => (
-          <DictionaryItem key={dictionary.name} dictionary={dictionary} />
+          <DictionaryItem
+            key={dictionary.name}
+            dictionary={dictionary}
+            handleSelect={() => setSelectedDictionary(dictionary)}
+          />
         ))}
       </SidebarContent>
     </>
@@ -21,9 +27,10 @@ export function DictionaryManager() {
 
 interface DictionaryItemProps {
   dictionary: DictionaryMinimal;
+  handleSelect: () => void;
 }
 
-function DictionaryItem({ dictionary }: DictionaryItemProps) {
+function DictionaryItem({ dictionary, handleSelect }: DictionaryItemProps) {
   return (
     <div className="relative text-sm">
       <button
@@ -31,6 +38,7 @@ function DictionaryItem({ dictionary }: DictionaryItemProps) {
         className={cn(
           'hover:bg-muted flex w-full flex-col gap-2 rounded-sm border p-2 text-start hover:cursor-pointer'
         )}
+        onClick={handleSelect}
       >
         <span>{dictionary.name}</span>
         <span className="text-muted-foreground text-xs">
