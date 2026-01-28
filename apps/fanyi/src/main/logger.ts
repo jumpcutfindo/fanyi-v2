9576e261bfaf50e6c945103aca7c53e4230d644b
jumpcutfindo <danielhoe97@gmail.com>
2026-01-28 11:53:57 +0800

    chore: replace pino with electron-log

--- apps/fanyi/src/main/logger.ts
+++ apps/fanyi/src/main/logger.ts
@@ -1,11 +1,9 @@
-import pino from 'pino';
+import log from 'electron-log/main';
 
-export const logger = pino({
-  level: 'trace',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-    },
-  },
-});
+log.initialize();
+
+log.transports.console.useStyles = true;
+log.transports.console.format =
+  '[{y}-{m}-{d} {h}:{i}:{s}.{ms}] %c[{level}]%c{text}';
+
+export const logger = log;
